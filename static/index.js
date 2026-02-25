c9de5973a19b85e5fc722814f5038e2753b5a5a1
Micah456 <98129716+Micah456@users.noreply.github.com>
2026-02-25 23:10:26 +0000

    Implemented create a task
    Created new task dialog and form. Created index.js. Implemented input collection to produce a task object. Styled dialog and form

---
+++ static/index.js
@@ -0,0 +1,57 @@
+const newTaskBtn = document.getElementById("new-task-btn")
+const newTaskDialog = document.getElementById("new-task-dialog")
+const newTaskForm = document.getElementById("new-task-form")
+const deadlineCheckbox = document.getElementById("deadline-checkbox")
+const deadlineInput = document.getElementById("deadline-input")
+const cancelNewTaskBtn = document.getElementById("cancel-new-task-btn")
+const taskNameInput = document.getElementById("task-name-input")
+const taskDescriptionTA = document.getElementById("task-description-ta")
+
+deadlineInput.setAttribute('min', (new Date()).toISOString().split('T')[0])
+
+newTaskBtn.addEventListener("click", () => {
+    if(!newTaskDialog.open){
+        newTaskForm.reset()
+        newTaskDialog.open = true
+    }
+})
+
+deadlineCheckbox.addEventListener("change", () => {
+    if (deadlineCheckbox.checked){
+        deadlineInput.disabled = false
+    }else{
+        deadlineInput.value = ""
+        deadlineInput.disabled = true
+    }
+})
+
+cancelNewTaskBtn.addEventListener('click', closeNewTaskDialog)
+
+newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const taskName = taskNameInput.value.trim()
+    const taskDescription = taskDescriptionTA.value.trim()
+    const deadline = deadlineInput.value
+    const task = {}
+    if(taskName){
+        task.ID = Number(new Date())
+        task.Name = taskName
+        task.Description = taskDescription
+        task.Deadline = deadline ? Number(new Date(deadline)) : -1
+        task.Done = false
+        saveTask(task)
+        closeNewTaskDialog()
+        
+    }else{
+        alert("Could not save task: task name required")
+    }
+})
+
+function saveTask(task){
+    console.log(task)
+}
+
+function closeNewTaskDialog(){
+    newTaskForm.reset()
+    newTaskDialog.open = false
+}
